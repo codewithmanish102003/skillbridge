06bf14e71380e7f6ef6b4feebd762ea03c903a35
Manish Prajapati <marveluniverse1942@gmail.com>
2025-07-18 20:55:54 +0530

    Project Initialization

---
+++ app/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="Footer flex items-center justify-between w-full p-4 bg-gray-600 shadow-md">
+        <div className="flex items-center gap-4 text-[#0d0f1c]">
+            footer content  
+
+            </div>
+    </div>
+  )
+}
+
+export default Footer 
